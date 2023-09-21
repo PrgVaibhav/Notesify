83fbf404cc018a68a9c5ee48aa5aed68a2a13009
prgVaibhav <vaibhav2k1507@gmail.com>
2023-09-21 18:46:06 +0530

    Improved my footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,17 @@ import styles from "./footer.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Created by Vaibhav with ❤</p>
+      <p>
+        Created by{" "}
+        <a
+          href="https://kumarvaibhav.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Vaibhav
+        </a>{" "}
+        with ❤
+      </p>
     </footer>
   );
 };
